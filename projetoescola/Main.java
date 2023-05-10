230ad0d0866c548e33fd08cbd239a824c9433b56
William Torres <wiliam.wgtm@gmail.com>
2023-05-10 20:56:37 -0300

    Estrutura inicial do projeto escola

---
+++ projetoescola/Main.java
@@ -0,0 +1,11 @@
+package projetoescola;
+
+public class Main {
+    public static void main(String[] args) {
+//TODO: CRIAR CLASSE FUNCIONÁRIO COM AS SEGUINTES VARIAVEIS(nome,idade,cpf,cargo,salario)
+        //TODO: CRIAR CLASSE ALUNO COM AS SEGUINTES VARIAVEIS(nome,idade,cpf,matricula,nota)
+        //TODO: CRIAR CLASSE PROFESSOR COM AS SEGUINTES VARIAVEIS(matéria) e estender a classe funcionario para a classe professor
+        //TODO: CRIAR CLASSE CronogramaAlimentarCantina(vocês escolhem as variaveis) e também criar uma função que ao chamar a mesma ela retorna a seguinte frase "HORA DO RANGO"
+        //TODO: CRIAR MÉTODO PARA CALCULAR A MÉDIA DE UM ALUNO
+    }
+}
